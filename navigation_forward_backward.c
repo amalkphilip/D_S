72f898899836590197af9ca9c0a6550b7eb91ae4
Amal K Philip <amal17philip@gmail.com>
2025-10-24 00:02:40 +0530

    navigation functions and main loop

--- navigation_forward_backward.c
+++ navigation_forward_backward.c
@@ -7,20 +7,16 @@ struct node {
     struct node* next;   
     struct node* prev;    
 };
-
 typedef struct node Node;
-
 void create(Node** head, char* url) {
     Node* newnode = (Node*)malloc(sizeof(Node));
     if (newnode == NULL) {
         printf("Memory allocation failed!\n");
         return;
     }
-
     strcpy(newnode->data, url);
     newnode->next = NULL;
     newnode->prev = NULL;
-
     if (*head == NULL) {
         *head = newnode;  
     } else {
@@ -32,7 +28,6 @@ void create(Node** head, char* url) {
         newnode->prev = temp;  
     }
 }
-
 void printCurrent(Node* current) {
     if (current != NULL) {
         printf("Currently viewing: %s\n", current->data);
@@ -40,7 +35,6 @@ void printCurrent(Node* current) {
         printf("No webpage to view!\n");
     }
 }
-
 void goForward(Node** current) {
     if (*current != NULL && (*current)->next != NULL) {
         *current = (*current)->next;
@@ -49,7 +43,6 @@ void goForward(Node** current) {
         printf(" You are at the last page.\n");
     }
 }
-
 void goBackward(Node** current) {
     if (*current != NULL && (*current)->prev != NULL) {
         *current = (*current)->prev;
@@ -58,7 +51,6 @@ void goBackward(Node** current) {
         printf(" You are at the first page.\n");
     }
 }
-
 void clearHistory(Node** head) {
     Node* temp = *head;
     while (temp != NULL) {
@@ -69,17 +61,11 @@ void clearHistory(Node** head) {
     *head = NULL;  
     printf("\n\t\tSAFE ayiee mwoneee......\n");
 }
-
-	
-
-
 int main() {
     Node* head = NULL;  
     Node* current = NULL;  
-
     int choice;
     char url[100];
-
     while (1) {
         printf("\n---Browser Navigation Menu---\n");
         printf("1. Visit a webpage\n");
@@ -89,44 +75,31 @@ int main() {
         printf("5. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
-
         switch (choice) {
             case 1:
                 printf("Enter the URL to visit: ");
                 scanf("%s", url);
                 create(&head, url);  
-
                 if (current == NULL) {
                     current = head;
                 }
-
                 printCurrent(current);  
                 break;
-
             case 2:
                 goForward(&current); 
                 break;
-
             case 3:
                 goBackward(&current);  
                 break;
-                
             case 4:
             	clearHistory(&head);
             	break;
-
             case 5:
                 printf("Exiting...\n");
                 return 0;
-
             default:
                 printf("Invalid choice! Please try again.\n");
         }
-        
     }
-    
-    
-
     return 0;
 }
-
